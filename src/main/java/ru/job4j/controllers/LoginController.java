e611aaef5fe9fb6d60cc8a829708ff37545ef037
Windows <ivinjc@gmail.com>
2022-08-21 10:33:49 +0300

    Spring Boot [#6880 #334434]

--- src/main/java/ru/job4j/controllers/LoginController.java
+++ src/main/java/ru/job4j/controllers/LoginController.java
@@ -1,7 +1,5 @@
 package ru.job4j.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -12,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class LoginController {
     @GetMapping("/login")
@@ -19,20 +20,20 @@ public class LoginController {
                             @RequestParam(value = "logout", required = false) String logout,
                             Model model) {
         String errorMessage = null;
-        if(error != null) {
+        if (error != null) {
             errorMessage = "Username or Password is incorrect !!";
         }
-        if(logout != null) {
+        if (logout != null) {
             errorMessage = "You have been successfully logged out !!";
         }
         model.addAttribute("errorMessge", errorMessage);
         return "login";
     }
 
-    @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
+        if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/login?logout=true";
