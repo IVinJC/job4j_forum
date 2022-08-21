e47fcae7ae938f2dcc295b0d579aee982196eb7b
Windows <ivinjc@gmail.com>
2022-08-21 14:44:09 +0300

    Spring test [#6881]

--- src/test/java/ru/job4j/controllers/IndexControllerTest.java
+++ src/test/java/ru/job4j/controllers/IndexControllerTest.java
@@ -5,8 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
 
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/ru/job4j/controllers/RegControllerTest.java
+++ src/test/java/ru/job4j/controllers/RegControllerTest.java
@@ -1,7 +1,7 @@
 package ru.job4j.controllers;
 
 import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
